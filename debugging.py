a740cd7f3b796247c265ec1980898d7eb20547b5
Alvaro Barrera <alvaro_bv@hotmail.com>
2021-04-10 19:15:17 -0500

    Debugging

---
+++ debugging.py
@@ -0,0 +1,17 @@
+def divisors(num):
+    divisors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            if num % i == 0:
+                divisors.append(i)
+    return divisors
+
+def run():
+    try:
+        num = int(input("Ingresa un número: "))
+        print(divisors(num))
+    except ValueError:
+        print("Debes ingresar un número")
+
+if __name__ == '__main__':
+    run()
